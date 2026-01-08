bd901c079c93dd0869ce71b47cbaed1bd4cef775
Iker-Jauregui <iker.jauregui.elso@gmail.com>
2026-01-08 18:07:34 +0100

    Scalability Study

--- scripts/scalability_worker.py
+++ scripts/scalability_worker.py
@@ -1,38 +1,42 @@
-# scalability_worker.py (Updated)
+import os
+import hashlib
 import sys
 import time
-from pyspark.sql import SparkSession
+import pyspark.sql.functions as sql_f
 from multiprocessing.pool import ThreadPool
 from pyspark import keyword_only
+from pyspark.sql import SparkSession
 from pyspark.ml.feature import StringIndexer, VectorAssembler
 from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml import Pipeline
-import time
-import sys
-import os
+
 
 """
     UK Police Stop and Search Scalability Tool
     
-    Param 1: Number of Cores/Partitions
-    Param 2: Percentage of data to use (1-100)
-    Param 3: Filename to save the results (CSV)
+    Param 1: Scalability study type ('size-up', 'speed-up' or 'scale-up')
+    Param 2: Number of times the scalability study will be repeated
+    Param 3: Number of Cores/Partitions
+    Param 4: Percentage of data to use (1-100)
+    Param 5: Filename to save the results (CSV)
 """
 
 def main(argv):
     # 1. Parse Arguments
-    if len(argv) < 3:
-        print("Usage: python scalability_worker.py <cores> <percentage> <results_file>")
+    if len(argv) < 5:
+        print("Usage: python scalability_worker.py <study_type> <times> <cores> <percentage> <results_file>")
         sys.exit(1)
 
-    cores = int(argv[0])
-    percentage = int(argv[1]) / 100.0  # Convert 10 to 0.1
-    filename = argv[2]
+    study_type = argv[0]
+    times = int(argv[1])
+    cores = int(argv[2])
+    percentage = int(argv[3]) / 100.0
+    filename = argv[4]
+
+    # Define study id
+    study_id = hashlib.md5(str(time.time()).encode()).hexdigest()
 
     # 2. Initialize Spark Session
-    from pyspark.sql import SparkSession
-    import pyspark.sql.functions as sql_f
-
     spark = (
         SparkSession.builder.master(f"local[{cores}]")
         .appName(f"UK_Police_Scalability_{cores}cores_{int(percentage*100)}perc")
@@ -57,54 +61,90 @@ def main(argv):
     # Trigger lazy operations before timing
     df.count()
 
-    # 4. Start Measuring Training Phase
-    start = time.time()
-  
-    from pyspark.ml.feature import VectorAssembler, StringIndexer
-    from pyspark.ml.classification import DecisionTreeClassifier
-    from pyspark.ml import Pipeline
+    # 4. Warm-up runs <- not logged
+    for _ in range(3):
 
-    # Define our specific columns
-    categorical_cols = ["age", "gender", "law", "ethnicity", "search_type", "reason", "police_force"]
-    numeric_cols = ["is_person", "strip_search", "hour", "month", "dayofweek"]
+        # Start Measuring Training Phase
+        start = time.time()
 
-    # Preprocessing: StringIndexers for categories
-    indexers = [
-        StringIndexer(inputCol=c, outputCol=f"{c}_idx", handleInvalid="keep") 
-        for c in categorical_cols
-    ]
+        # Define our specific columns
+        categorical_cols = ["age", "gender", "law", "ethnicity", "search_type", "reason", "police_force"]
+        numeric_cols = ["is_person", "strip_search", "hour", "month", "dayofweek"]
 
-    # Assemble all features
-    feature_indices = [f"{c}_idx" for c in categorical_cols] + numeric_cols
-    assembler = VectorAssembler(inputCols=feature_indices, outputCol="features", handleInvalid="keep")
+        # Preprocessing: StringIndexers for categories
+        indexers = [
+            StringIndexer(inputCol=c, outputCol=f"{c}_idx", handleInvalid="keep") 
+            for c in categorical_cols
+        ]
+
+        # Assemble all features
+        feature_indices = [f"{c}_idx" for c in categorical_cols] + numeric_cols
+        assembler = VectorAssembler(inputCols=feature_indices, outputCol="features", handleInvalid="keep")
+        
+        # Decision Tree
+        # Note: maxBins=64 handles the 42 police forces in your data
+        dt = DecisionTreeClassifier(
+            labelCol="legal_action_taken", 
+            featuresCol="features", 
+            maxBins=64
+        ) 
+
+        # Building the Pipeline (Preprocessing + Training included)
+        pipeline = Pipeline(stages=indexers + [assembler, dt])
+
+        # Fitting the model
+        pipeline_model = pipeline.fit(df)
+
+        end = time.time()
+        runtime = end - start
+
+    # 5. Perform study multiple times
+    for run_id in range(times):
+
+        # Start Measuring Training Phase
+        start = time.time()
     
-    # Decision Tree
-    # Note: maxBins=64 handles the 42 police forces in your data
-    dt = DecisionTreeClassifier(
-        labelCol="legal_action_taken", 
-        featuresCol="features", 
-        maxBins=64
-    ) 
+        # Define our specific columns
+        categorical_cols = ["age", "gender", "law", "ethnicity", "search_type", "reason", "police_force"]
+        numeric_cols = ["is_person", "strip_search", "hour", "month", "dayofweek"]
 
-    # Building the Pipeline (Preprocessing + Training included)
-    pipeline = Pipeline(stages=indexers + [assembler, dt])
+        # Preprocessing: StringIndexers for categories
+        indexers = [
+            StringIndexer(inputCol=c, outputCol=f"{c}_idx", handleInvalid="keep") 
+            for c in categorical_cols
+        ]
 
-    # Fitting the model
-    pipeline_model = pipeline.fit(df)
+        # Assemble all features
+        feature_indices = [f"{c}_idx" for c in categorical_cols] + numeric_cols
+        assembler = VectorAssembler(inputCols=feature_indices, outputCol="features", handleInvalid="keep")
+        
+        # Decision Tree
+        # Note: maxBins=64 handles the 42 police forces in your data
+        dt = DecisionTreeClassifier(
+            labelCol="legal_action_taken", 
+            featuresCol="features", 
+            maxBins=64
+        ) 
 
-    end = time.time()
-    runtime = end - start
+        # Building the Pipeline (Preprocessing + Training included)
+        pipeline = Pipeline(stages=indexers + [assembler, dt])
 
-    # 5. Output Results
-    print(f"Building phase took: {runtime} seconds")    
-    
-    # Write to the CSV file provided in arguments
-    file_exists = os.path.isfile(filename)
-    with open(filename, "a") as f:
-        # If file is new, add header
-        if not file_exists:
-            f.write("cores,percentage,runtime\n")
-        f.write(f"{cores},{int(percentage*100)},{runtime}\n")
+        # Fitting the model
+        pipeline_model = pipeline.fit(df)
+
+        end = time.time()
+        runtime = end - start
+
+        # Output Results
+        print(f"Building phase took: {runtime} seconds")    
+        
+        # Write to the CSV file provided in arguments
+        file_exists = os.path.isfile(filename)
+        with open(filename, "a") as f:
+            # If file is new, add header
+            if not file_exists:
+                f.write("study_type,study_id,run_id,cores,percentage,runtime\n")
+            f.write(f"{study_type},{study_id},{run_id},{cores},{int(percentage*100)},{runtime}\n")
             
     spark.stop()
     
